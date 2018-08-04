b7b9dcd7f44ef4d842d9abf618b80dbbc688e164
irfanmumtaz <im.irfanmumtaz21@gmail.com>
2018-08-04 14:04:38 +0500

    filtering extra things

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
 from flask_pymongo import PyMongo
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
 from bson.json_util import dumps
 
 import time
@@ -87,53 +84,5 @@ def index():
 def logout():
     logout_user()
     return 'Logged out'
-
-
-
-# @app.route('/')
-# def index():
-#     if 'username' in session:
-#         return 'you are logged in as ' + session['username']
-    
-#     return render_template('login.html')
-
-
-# @app.route('/login', methods=['POST'])
-# def login():
-#     user = mongo.db.users
-#     result = user.find_one({'email': request.form['email']})
-#     if result:
-#         verify = check_password_hash(result['password'], generate_password_hash(request.form['password']))
-#         return dumps(verify)
-#         if verify:
-#             session['username'] = request.form['email']
-#             return redirect(url_for('index'))
-#         return '<h2>Invalid password credentials</h2>'
-            
-#     return '<h2>Invalid user credentials</h2>'
-
-# @app.route('/register', methods=['POST', 'GET'])
-# def register():
-#     if request.method == 'POST':
-#         users = mongo.db.users
-#         result = users.find_one({'email': request.form['email']})
-#         if result is None:
-
-#             form = {
-#                 'name': request.form['name'],
-#                 'email': request.form['email'],
-#                 'password': bcrypt.generate_password_hash(request.form['password']),
-#                 'created_at': time.strftime('%d-%m-%Y %H:%M:%S'),
-#                 'updated_at': time.strftime('%d-%m-%Y %H:%M:%S')
-#             }   
-
-#             users.insert(form)
-#             session['username'] = request.form['email']
-#             return redirect(url_for('index'))
-        
-#         return '<h2>This user is already exist, please try another</h2>'
-#     return render_template('registration.html')
-
-    
 
 app.run(debug=True)
